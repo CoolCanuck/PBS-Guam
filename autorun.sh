96a8e99e8a37a830ac99ddc595df8f1e48dad1c5
CoolCanuck <CoolCanuck@users.noreply.github.com>
2023-04-16 22:48:11 -0400

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -9,6 +9,11 @@ cd $(dirname $0)/scripts/
 python3 script.py https://www.youtube.com/@thenewclassicalfm/live > ../streams/classicalfm.m3u8
 python3 script.py https://www.youtube.com/@zoomerradiohits/live > ../streams/zoomerradio.m3u8
 python3 script.py https://www.youtube.com/user/pbsguam/live > ../streams/pbsguam.m3u8
+python3 script.py https://www.youtube.com/watch?v=b-PyEswUenE > ../streams/theworldlive.m3u8
+python3 script.py https://www.youtube.com/watch?v=3JphXZPvhJA > ../streams/cam-birdfeeder-ontario.m3u8
+python3 script.py https://www.youtube.com/watch?v=N609loYkFJo > ../streams/cam-birdfeeder-sapsucker-ny.m3u8
+
+
 
 
 
