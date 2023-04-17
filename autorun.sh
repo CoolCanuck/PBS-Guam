ed6d892ef182158ce648ce1f8ba378abeebbe95d
CoolCanuck <CoolCanuck@users.noreply.github.com>
2023-04-16 21:15:10 -0400

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -6,7 +6,11 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 script.py https://www.youtube.com/@thenewclassicalfm/live > ../classicalfm.m3u8
-python3 script.py https://www.youtube.com/@zoomerradiohits/live > ../zoomerradio.m3u8
+python3 script.py https://www.youtube.com/@thenewclassicalfm/live > ../streams/classicalfm.m3u8
+python3 script.py https://www.youtube.com/@zoomerradiohits/live > ../streams/zoomerradio.m3u8
+python3 script.py https://www.youtube.com/user/pbsguam/live > ../streams/pbsguam.m3u8
+
+
+
 
 echo m3u grabbed
