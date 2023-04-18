672454acf68252b20f9cd7ddb650962c984ae01e
CoolCanuck <CoolCanuck@users.noreply.github.com>
2023-04-17 21:00:53 -0400

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -12,10 +12,11 @@ python3 script.py https://www.youtube.com/user/pbsguam/live > ../streams/pbsguam
 python3 script.py https://www.youtube.com/watch?v=b-PyEswUenE > ../streams/theworldlive.m3u8
 python3 script.py https://www.youtube.com/watch?v=3JphXZPvhJA > ../streams/cam-birdfeeder-ontario.m3u8
 python3 script.py https://www.youtube.com/watch?v=N609loYkFJo > ../streams/cam-birdfeeder-sapsucker-ny.m3u8
-
-
-
-
+python3 script.py https://www.youtube.com/watch?v=BahXDYWQAKk > ../streams/cam-railroad-revelstoke-bc.m3u8
+python3 script.py https://www.youtube.com/watch?v=S26YOMAD290 > ../streams/cam-airport-MDW-il.m3u8
+python3 script.py https://www.youtube.com/watch?v=kxwuFPFUZyY > ../streams/cam-views-niagarafalls-on.m3u8
+python3 script.py https://www.youtube.com/watch?v=WBvHHNXXeqw > ../streams/cam-beach-ftlauderdale-fl.m3u8
+python3 script.py https://www.youtube.com/watch?v=EPKWu223XEg> ../streams/cam-city-collingwood-on.m3u8
 
 
 echo m3u grabbed
