536507c9407124dd39cc4cdc376b8140b6ea4665
CoolCanuck <CoolCanuck@users.noreply.github.com>
2023-04-18 12:16:39 -0400

    Create autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -6,7 +6,7 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 script.py https://www.youtube.com/@thenewclassicalfm/live > ../streams/classicalfm.m3u8
+python3 script.py https://www.youtube.com/@thenewclassicalfm/live > ../_redirects
 python3 script.py https://www.youtube.com/@zoomerradiohits/live > ../streams/zoomerradio.m3u8
 python3 script.py https://www.youtube.com/user/pbsguam/live > ../streams/pbsguam.m3u8
 python3 script.py https://www.youtube.com/watch?v=b-PyEswUenE > ../streams/theworldlive.m3u8
